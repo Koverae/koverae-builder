b95182ea2972ef146aec67f9bf743a8b030b73ff
Arden28 <laudbouetoumoussa@gmail.com>
2024-10-29 16:10:08 +0100

    Added module option to page maker command

--- src/Traits/CommandHelper.php
+++ src/Traits/CommandHelper.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Str;
 
 trait CommandHelper
 {
-    protected function isCustomModule()
+    protected function isModule()
     {
-        return $this->option('custom') === true;
+        return !empty($this->option('module'));
     }
 
     protected function isForce()
@@ -58,24 +58,6 @@ trait CommandHelper
     {
         $moduleName = $this->argument('module');
 
-        if ($this->isCustomModule()) {
-            $module = config("modules-livewire.custom_modules.{$moduleName}");
-
-            $path = $module['path'] ?? '';
-
-            if (! $module || ! File::isDirectory($path)) {
-                $this->line("<options=bold,reverse;fg=red> WHOOPS! </> 😳 \n");
-
-                $path && $this->line("<fg=red;options=bold>The custom {$moduleName} module not found in this path - {$path}.</>");
-
-                ! $path && $this->line("<fg=red;options=bold>The custom {$moduleName} module not found.</>");
-
-                return null;
-            }
-
-            return $moduleName;
-        }
-
         if (! $module = $this->laravel['modules']->find($moduleName)) {
             $this->line("<options=bold,reverse;fg=red> WHOOPS! </> 😳 \n");
             $this->line("<fg=red;options=bold>The {$moduleName} module not found.</>");
@@ -88,16 +70,12 @@ trait CommandHelper
 
     protected function getModuleName()
     {
-        return $this->isCustomModule()
-            ? $this->module
-            : $this->module->getName();
+        return $this->module->getName();
     }
 
     protected function getModuleLowerName()
     {
-        return $this->isCustomModule()
-            ? config("modules-livewire.custom_modules.{$this->module}.name_lower", strtolower($this->module))
-            : $this->module->getLowerName();
+        return $this->module->getLowerName();
     }
 
     protected function getModulePath()
@@ -116,10 +94,6 @@ trait CommandHelper
     {
         $moduleLivewireNamespace = config('modules-livewire.namespace', 'Http\\Livewire');
 
-        if ($this->isCustomModule()) {
-            return config("modules-livewire.custom_modules.{$this->module}.namespace", $moduleLivewireNamespace);
-        }
-
         return $moduleLivewireNamespace;
     }
 
@@ -130,9 +104,7 @@ trait CommandHelper
         // Determine the type of component: form, table, map, ...
         $type = ucfirst($this->getTypeInfo());
 
-        $prefix = $this->isCustomModule()
-            ? $this->getModuleNamespace().'\\'.$this->getModuleLivewireNamespace().'\\'.$type
-            : $this->getModuleNamespace().'\\'.$this->module->getName().'\\'.$this->getModuleLivewireNamespace().'\\'.$type;
+        $prefix = $this->getModuleNamespace().'\\'.$this->module->getName().'\\'.$this->getModuleLivewireNamespace().'\\'.$type;
 
         return (string) Str::of($classPath)
             ->beforeLast('/')
@@ -143,10 +115,6 @@ trait CommandHelper
     protected function getModuleLivewireViewDir()
     {
         $moduleLivewireViewDir = config('modules-livewire.view', 'Resources/views/livewire');
-
-        if ($this->isCustomModule()) {
-            $moduleLivewireViewDir = config("modules-livewire.custom_modules.{$this->module}.view", $moduleLivewireViewDir);
-        }
 
         return $this->getModulePath().'/'.$moduleLivewireViewDir;
     }
